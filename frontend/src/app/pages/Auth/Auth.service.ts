a155896d2c4cff80347d3f2d3685a1a67c4b1f44
GeraldMatarrita <geramatarr@gmail.com>
2024-10-17 20:25:52 -0600

    Profile update

--- frontend/src/app/pages/Auth/Auth.service.ts
+++ frontend/src/app/pages/Auth/Auth.service.ts
@@ -172,47 +172,39 @@ export class AuthService {
       );
     });
   }
+    // Método para actualizar los datos del usuario activo
+    updateActiveUser(updatedUser: any): void {
+      this.activeUser = updatedUser;
+      this.activeUserID = updatedUser._id;
   
-  // Método para actualizar los datos del usuario activo
-  updateActiveUser(updatedUser: any): void {
-    this.activeUser = updatedUser;
-    this.activeUserID = updatedUser._id;
+      // Guardar los nuevos datos en localStorage
+      localStorage.setItem('token', JSON.stringify(updatedUser));
+  
+      // Actualizar los observables si es necesario
+      this.userNameSubject.next(updatedUser.realName);
+    }
+  
+    // Método para manejar el update de los datos del usuario en el backend
+    updateUserInBackend(userID: string, data: any): Promise<{ message: string; user: any }> {
+      return new Promise((resolve, reject) => {
+        this.http.put<{ message: string; user: any }>(`${API_ROUTES.BASE_URL}${API_ROUTES.Update_User}/${userID}`, data)
+          .subscribe(
+            (response) => {
+              // Actualiza el usuario activo
+              this.updateActiveUser(response.user);
+              resolve(response);
+            },
+            (error) => {
+              console.error('Error updating user in backend:', error);
+              reject(error);
+            }
+          );
+      });
+    }
 
-    // Guardar los nuevos datos en localStorage
-    localStorage.setItem('token', JSON.stringify(updatedUser));
-
-    // Actualizar los observables si es necesario
-    this.userNameSubject.next(updatedUser.name);
-  }
-
-  // Método para manejar el update de los datos del usuario en el backend
-  updateUserInBackend(
-    userID: string,
-    data: any
-  ): Promise<{ message: string; user: any }> {
-    return new Promise((resolve, reject) => {
-      this.http
-        .put<{ message: string; user: any }>(
-          `${API_ROUTES.BASE_URL}${API_ROUTES.Update_User}/${userID}`,
-          data
-        )
-        .subscribe(
-          (response) => {
-            // Actualiza el usuario activo
-            this.updateActiveUser(response.user);
-            resolve(response);
-          },
-          (error) => {
-            console.error('Error updating user in backend:', error);
-            reject(error);
-          }
-        );
-    });
-  }
-
-  async getActiveUserName(): Promise<{ name: string }> {
+  async getActiveUserName(): Promise<{ realName: string }> {
     // Retornar el usuario activo desde tu lógica
-    return { name: this.userNameSubject.getValue() };
+    return { realName: this.userNameSubject.getValue() };
   }
 
   requestPasswordReset(data: { email: string }): Observable<any> {
